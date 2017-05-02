c2bf7592849750cd92dacc187fa5b60e82b277da
Dennis Fedorchuk <dennisfedorchuk@gmail.com>
2017-05-02 02:32:17 -0400

    Created an update method. Delete and incert do not work for some reason

--- src/java/music/products/ProductIO.java
+++ src/java/music/products/ProductIO.java
@@ -2,15 +2,9 @@ package music.products;
 
 
 
-import java.io.*;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
@@ -19,7 +13,7 @@ public class ProductIO {
     private static List<Product> products = null;
    
     public static List<Product> selectProducts() {
-         EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
         String qString = "SELECT p from Product p";
         TypedQuery<Product> q = em.createQuery(qString, Product.class);
         List<Product> results = null;
@@ -52,69 +46,44 @@ public class ProductIO {
     }    
 
     public static void insertProduct(Product product) {
-        /*ConnectionPool pool = ConnectionPool.getInstance();
-        Connection connection = pool.getConnection();
-        PreparedStatement ps = null;
-        
-        String query = "INSERT INTO product(ProductCode, ProductDescription, ProductPrice)" 
-                + " VALUES (?, ?, ?)";
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();
         try{
-            ps = connection.prepareStatement(query);
-            ps.setString(1, product.getCode());
-            ps.setString(2, product.getDescription());
-            ps.setDouble(3, product.getPrice());
-            
-            ps.executeUpdate();
-        }catch(SQLException e){
-            System.out.println(e);
+            em.persist(product);
+            trans.commit();
+        } catch (Exception e){
+            trans.rollback();
         } finally {
-            DBUtil.closePreparedStatement(ps);
-            pool.freeConnection(connection);
-        }*/
+            em.close();
+        }
     }
 
     public static void updateProduct(Product product) {
-       /*ConnectionPool pool = ConnectionPool.getInstance();
-       Connection connection = pool.getConnection();
-       PreparedStatement ps = null;
-       
-       String query = "UPDATE product " 
-                + " SET ProductDescription = ?, ProductPrice = ? "
-                + " WHERE ProductCode = ?";
-   
-       try {
-           ps = connection.prepareStatement(query);
-           ps.setString(1, product.getDescription());
-           ps.setDouble(2, product.getPrice());
-           ps.setString(3, product.getCode());
-                          
-           ps.executeUpdate();
-       } catch (SQLException e) {
-           System.out.println(e);
-       } finally {
-           DBUtil.closePreparedStatement(ps);
-           pool.freeConnection(connection);
-       }*/
-      
+       EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();
+        try{
+            em.merge(product);
+            trans.commit();
+        } catch (Exception e){
+            trans.rollback();
+        } finally {
+            em.close();
+        }      
     }
 
     public static void deleteProduct(Product product) {
-        /*ConnectionPool pool = ConnectionPool.getInstance();
-        Connection connection = pool.getConnection();
-        PreparedStatement ps = null;
-        
-        String query = "DELETE FROM product" 
-                + " WHERE ProductCode = ?";
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();
         try{
-            ps = connection.prepareStatement(query);
-            ps.setString(1, product.getCode());
-            
-            ps.executeUpdate();
-        }catch(SQLException e){
-            System.out.println(e);
+            em.remove(product);
+            trans.commit();
+        } catch (Exception e){
+            trans.rollback();
         } finally {
-            DBUtil.closePreparedStatement(ps);
-            pool.freeConnection(connection);
-        }*/
+            em.close();
+        }
     }    
 }
